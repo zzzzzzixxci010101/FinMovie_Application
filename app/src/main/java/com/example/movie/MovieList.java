e37957e88056d29c9dbe72058ff12ee3b94025cc
zzzzzzixxci010101 <jyyyyyyh06@naver.com>
2020-12-06 16:42:38 +0900

    update application

---
+++ app/src/main/java/com/example/movie/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.movie;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/movie/MovieListResult.java
@@ -0,0 +1,13 @@
+package com.example.movie;
+
+import java.util.ArrayList;
+
+public class MovieListResult {
+
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+
+}
+
